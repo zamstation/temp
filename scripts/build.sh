06f96d7a8d8f34d260d4b6b86f07f6672ba9a099
amsakanna <amsakanna@gmail.com>
2021-06-06 16:05:54 +0530

    build script 5

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,8 @@
 remoteDir="https://raw.githubusercontent.com/zamstation/dart_package_template/main/scripts"
-localDir=".temp"
+localDir="./.temp"
 mkdir -p "$localDir"
-pwd
 ls -l
-cd -
-ls -l
+ls -l $localDir
 echo "Downloading Scripts ..."
 curl -s -o "$localDir/logger.sh" "$remoteDir/logger.sh"
 curl -s -o "$localDir/error_thrower.sh" "$remoteDir/error_thrower.sh"
@@ -12,4 +10,4 @@ curl -s -o "$localDir/test.sh" "$remoteDir/test.sh"
 echo "Running Scripts ..."
 sh "$localDir/test.sh"
 echo "Removing Scripts ..."
-rm -rf ".temp"
+rm -rf "$localDir"

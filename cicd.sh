e8a59dcda8833d21d620ba0ae4e543d38902b53a
amsakanna <amsakanna@gmail.com>
2021-06-06 17:32:48 +0530

    build script 24

--- cicd.sh
+++ cicd.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
 set -e
+
 remoteDir="https://raw.githubusercontent.com/zamstation/dart_package_template/main/scripts"
 localDir=".temp"
+
 mkdir -p "$localDir"
+
 echo "Downloading Scripts from $remoteDir to $localDir ..."
 curl -o "$localDir/logger.sh" "$remoteDir/logger.sh"
 curl -o "$localDir/error_thrower.sh" "$remoteDir/error_thrower.sh"
 curl -o "$localDir/validate.sh" "$remoteDir/validate.sh"
-# chmod -R 740 "$localDir"
+
+chmod -R 740 "$localDir"
 ls -l "$localDir"
+
 echo "Running Scripts ..."
-./"$localDir/validate.sh"
+sh "$localDir/validate.sh"
+
 echo "Removing Scripts ..."
 rm -rf "$localDir"

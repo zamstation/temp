1ad03aa7a602ebc1c3199407fc7ddfe4d9a7dcfb
amsakanna <amsakanna@gmail.com>
2021-06-06 19:53:17 +0530

    cicd test 26

---
+++ download.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+echo ""
+remoteDir="https://raw.githubusercontent.com/zamstation/dart_package_template/main/scripts"
+localDir=".temp"
+mkdir -p "$localDir"
+
+declare -a scripts
+while read -r scriptName; do
+	scripts[((i++))]="$scriptName"
+done < "script_list.txt"
+
+for script in "${scripts[@]}"; do
+	echo "Downloading $remoteDir/$script ..."
+	curl -s -o "$localDir/$script" "$remoteDir/$script"
+	chmod 700 "$localDir/$script"
+done
+
+echo -e "\nContents of $localDir:"
+ls -ltr "$localDir"
+
+echo -e "\nRunning Scripts ..."
+./"$localDir/validate.sh"
+
+echo "\nCleaning Up ..."
+rm -rf "$localDir"
+
+exit 0

96900708ad125135cd06dc39cfe18c4a3e1a1e18
amsakanna <amsakanna@gmail.com>
2021-06-06 22:55:10 +0530

    cicd test 34

--- a.sh
+++ a.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-set -e
 
-echo ""
-remoteDir="https://raw.githubusercontent.com/zamstation/dart_package_template/main/scripts"
+remoteUrl="https://raw.githubusercontent.com/zamstation/cicd_test/main"
+remoteDir="$remoteUrl/scripts"
 localDir="temp"
 mkdir -p "$localDir"
 
+echo -e "\nDownload URL: $remoteUrl"
+echo -e "\nReading script_list.txt ..."
 declare -a scripts
-while read -r scriptName; do
-	scripts[((i++))]="$scriptName"
-done <"script_list.txt"
+while read -r script; do
+	echo "$script"
+	scripts[((i++))]="$script"
+done < <(curl -s "$remoteUrl/script_list.txt")
 
+echo -e "\nDownloading Scripts ..."
 for script in "${scripts[@]}"; do
-	echo "Downloading $remoteDir/$script ..."
+	echo "$script"
 	curl -s -o "$localDir/$script" "$remoteDir/$script"
 	chmod 700 "$localDir/$script"
 done

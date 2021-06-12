92016c5d822a35cfea9f33b7ecfd57a1f39b4847
amsakanna <amsakanna@gmail.com>
2021-06-12 10:06:23 +0530

    test dry run

--- lib/src/serialize/deserialize.typedef.dart
+++ lib/src/serialize/deserialize.typedef.dart
@@ -1,4 +1,4 @@
 ///
 /// Alias for function that converts a map in to an object.
 ///
-typedef Deserialize<T extends Object?> = T Function(Map<String, Object?> json);
+typedef Deserialize<T extends Object?> = T Function(Map<String, Object?> json );

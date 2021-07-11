5df536035dc49afa2e44d2178923b86888cf804c
amsakanna <amsakanna@gmail.com>
2021-07-11 12:50:18 +0530

    12

--- scripts/list.sh
+++ scripts/list.sh
@@ -34,9 +34,9 @@ set +e
 # Gathering the list of new and modified Dockerfiles
 #
 logStep "Gathering the list of new and modified Dockerfiles"
-echo "GITHUB_BASE_REF: $GITHUB_BASE_REF"
+echo "GITHUB_REF: $GITHUB_REF"
 echo "GITHUB_SHA: $GITHUB_SHA"
-git diff --name-only "$GITHUB_BASE_REF..$GITHUB_SHA"
+git diff --name-only "$GITHUB_REF..$GITHUB_SHA"
 git diff-tree --no-commit-id --name-only -r HEAD..$COMMIT_SHA
 readarray -t dockerFiles < <(git diff-tree --no-commit-id --name-only -r $COMMIT_SHA | grep -sE "lib/.+/Dockerfile$")
 if [[ ${#dockerFiles[@]} -eq 0 ]]; then

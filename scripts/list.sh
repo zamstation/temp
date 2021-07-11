56772c709e060d16ac072662be842ab6d845dd8f
amsakanna <amsakanna@gmail.com>
2021-07-11 12:44:29 +0530

    8

--- scripts/list.sh
+++ scripts/list.sh
@@ -34,6 +34,8 @@ set +e
 # Gathering the list of new and modified Dockerfiles
 #
 logStep "Gathering the list of new and modified Dockerfiles"
+git diff --name-only "$GITHUB_BASE_REF..$GITHUB_SHA"
+git diff-tree --no-commit-id --name-only -r HEAD..$COMMIT_SHA
 readarray -t dockerFiles < <(git diff-tree --no-commit-id --name-only -r $COMMIT_SHA | grep -sE "lib/.+/Dockerfile$")
 if [[ ${#dockerFiles[@]} -eq 0 ]]; then
 	echo "No new docker file found."
